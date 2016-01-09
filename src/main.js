3960d8ab96d803f84050c460277656997fc0958f
Jonathan Ross <jonathan@jibo.com>
2016-01-09 16:53:46 -0500

    Order a hotel

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let factory = jibo.bt.factory;
 
 
 function start() {
-    let root = factory.create('../behaviors/16-hey-jibo');
+    let root = factory.create('../behaviors/17-make-reservations');
     root.start();
     let intervalId = setInterval(() => {
         if (root.status !== Status.IN_PROGRESS) {

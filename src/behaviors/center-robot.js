cc73f29d5421afa7efffea089c2de5000c6a82de
Jonathan Ross <jonathan@jibo.com>
2016-01-09 00:17:19 -0500

    Added custom behavior

--- src/behaviors/center-robot.js
+++ src/behaviors/center-robot.js
@@ -1,20 +1,20 @@
 "use strict";
 
 let jibo = require('jibo');
-
 let Status = jibo.bt.Status;
 let createBehavior = jibo.bt.createBehavior;
 let factory = jibo.bt.factory;
 
-
 module.exports = createBehavior({
-    constructor: function() {
+    constructor: function(isGlobal) {
+        this.isGlobal = isGlobal;
         this.status = Status.INVALID;
     },
     start: function() {
         this.status = Status.IN_PROGRESS;
         let dofs = jibo.animate.dofs;
-        jibo.animate.centerRobot(dofs.ALL, true, () => {
+        console.log(this.isGlobal);
+        jibo.animate.centerRobot(dofs.ALL, this.isGlobal, () => {
             this.status = Status.SUCCEEDED;
         });
         return true;

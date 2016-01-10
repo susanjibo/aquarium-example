f3acae1ba6af1a24dc7d71b3d396ef869f14db08
Jonathan Ross <jonathan@jibo.com>
2016-01-09 23:24:06 -0500

    changed range

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let factory = jibo.bt.factory;
 
 
 function start() {
-    let root = factory.create('../behaviors/17-make-reservations');
+    let root = factory.create('../behaviors/07-continuous-look-at');
     root.start();
     let intervalId = setInterval(() => {
         if (root.status !== Status.IN_PROGRESS) {

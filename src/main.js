52f082dd9b023e2ddc1a26f22adaa0075f5a9a98
Carolyn Saund <carolynsaund@jibo.com>
2016-01-11 14:21:17 -0500

    added lookat bt

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let factory = jibo.bt.factory;
 
 
 function start() {
-    let root = factory.create('../behaviors/test');
+    let root = factory.create('../behaviors/19-continuous-lookat');
     root.start();
     let intervalId = setInterval(() => {
         if (root.status !== Status.IN_PROGRESS) {
